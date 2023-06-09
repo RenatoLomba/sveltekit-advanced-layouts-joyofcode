29e9687e086de84aaf77fe92155b4e0191b5a441
RenatoLomba <rntlomba@gmail.com>
2023-06-09 18:00:32 -0300

    breaking out of layouts

---
+++ src/routes/(app)/quotes/(quote)/id/[quoteId]/+page@(app).svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+
+	export let data: PageData
+</script>
+
+<main class="space">
+	<h2 class="serif">Quote</h2>
+
+	<blockquote>
+		<p class="serif">“{data.quote.text}”</p>
+		<footer>
+			<cite>- {data.quote.author}</cite>
+		</footer>
+	</blockquote>
+</main>
