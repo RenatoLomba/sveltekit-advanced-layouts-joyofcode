4ccb3f3cdcd0d945aaa280c3f778beeaae402bb1
RenatoLomba <rntlomba@gmail.com>
2023-06-09 17:51:10 -0300

    feat: using group layouts

---
+++ src/routes/(dashboard)/admin/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { Visualize } from '$lib/devtools'
+</script>
+
+<Visualize layout="dashboard">
+	<header>
+		<nav>
+			<ul>
+				<li>
+					<a href="/">🏠️ Plume</a>
+				</li>
+				<li>
+					<a href="/admin/analytics">📊 Analytics</a>
+				</li>
+				<li>
+					<a href="/admin/settings">⚙️ Settings</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+</Visualize>
+
+<style>
+	header {
+		padding-inline: 2rem;
+		background: hsl(206 29% 6%);
+		border-bottom: 1px solid hsl(206 29% 12%);
+	}
+
+	main {
+		padding: 2rem;
+		background: hsl(206 29% 8%);
+		min-height: calc(100vh - 4rem);
+	}
+</style>
